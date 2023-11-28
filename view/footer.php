95c885cbea60743c4013951210fbe92e41d1cf0b
Nguyễn Văn Chỉnh <141124589+Chinhnv-ph37921@users.noreply.github.com>
2023-11-28 21:55:20 +0700

    duan1

---
+++ view/footer.php
@@ -0,0 +1,72 @@
+
+<!-- footer -->
+  <!-- Remove the container if you want to extend the Footer to full width. -->
+  <div class="wrap-footer">
+
+    <footer class="footer-distributed">
+
+      <div class="footer-left">
+
+        <h3>mobile<span>X</span></h3>
+
+        <p class="footer-links">
+          <a href="index.php" class="link-1">Trang chủ</a>
+
+          <a href="index.php?act=product">Sản phẩm</a>
+
+          <a href="index.php?act=introduce">Giới thiệu</a>
+
+          <a href="index.php?act=contact">Liên hệ</a>
+
+          <a href="index.php?act=question">Hỏi đáp</a>
+
+        </p>
+
+        <p class="footer-company-name">mobileX © 2023</p>
+      </div>
+
+      <div class="footer-center">
+
+        <div>
+          <i class="fa fa-map-marker"></i>
+          <p><span>Trịnh Văn Bô</span> Nam Từ Liêm, Hà Nội</p>
+        </div>
+
+        <div>
+          <i class="fa fa-phone"></i>
+          <p>+84 337686606</p>
+        </div>
+
+        <div>
+          <i class="fa fa-envelope"></i>
+          <p><a href="#">mobilex@gmail.com</a></p>
+        </div>
+
+      </div>
+
+      <div class="footer-right">
+        <p class="footer-company-about">
+          <span>Giới thiệu thương hiệu</span>
+          mobileX - Bán lẻ chính hãng Điện thoại di động. Hệ thống hơn 100 cửa hàng toàn quốc.
+          Mua mobileX giao tận nơi và tham khảo thêm nhiều sản phẩm khác. Miễn phí vận chuyển toàn quốc cho mọi đơn hàng . Đổi trả dễ dàng. Thanh toán bảo mật.
+        </p>
+
+        
+      </div>
+
+    </footer>
+
+  </div>
+  <!-- End of .container -->
+  <!-- Phần show nhanh chi tiết ngắn sản phẩm-->
+
+</div>
+<!-- end footer -->
+<script src="./src/js/plugins.min.js"></script>
+<script src="./src/js/ajax-mail.js"></script>
+<script src="./src/js/main.js"></script>
+</body>
+
+<!-- Mirrored from htmldemo.net/jobaria/jobaria/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 07 Nov 2022 14:13:15 GMT -->
+
+</html>
